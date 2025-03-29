33e9462947349055732ec3babd1fa2885a498753
ZhangXX <huitong.zh@outlook.com>
2025-03-29 14:14:36 +0800

    zht

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,71 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
 import ItemsView from '../views/ItemsView.vue'
+import GoodsView from '../views/Goods.vue'
+import MortgageView from '../views/Mortgage.vue'
 import OrdersView from '../views/OrdersView.vue'
+import UserLogin from '../views/Login.vue'
+import UserRegister from '../views/Register.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/items',
+    name: 'Items',
+    component: ItemsView
+  },
+  {
+    path: '/goods',
+    name: 'Goods',
+    component: GoodsView
+  },
+  {
+    path: '/mortgage',
+    name: 'Mortgage',
+    component: MortgageView
+  },
+  {
+    path: '/orders',
+    name: 'Orders',
+    component: OrdersView
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: UserLogin
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: UserRegister
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/items',
-      name: 'items',
-      component: ItemsView,
-    },
-    {
-      path: '/orders',
-      name: 'orders',
-      component: OrdersView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes
+})
+
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('token')
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!isAuthenticated) {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
