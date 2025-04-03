8c5029b5a5cb7905f1605459ceafb7187fd7209d
MonsterJuen <191955344@qq.com>
2025-04-03 20:37:39 +0800

    Add files via upload

---
+++ showpage/src/views/Login.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <h2>系统登录</h2>
+      <el-form :model="loginForm" :rules="loginRules" ref="loginForm">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="el-icon-user" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="el-icon-lock" type="password" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleLogin" class="login-button" :loading="loading">登录</el-button>
+          <el-button @click="goToRegister">注册账号</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+
+export default {
+  name: 'UserLogin',
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginRules: {
+        username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      },
+      loading: false
+    }
+  },
+  methods: {
+    async handleLogin() {
+      try {
+        await this.$refs.loginForm.validate()
+        this.loading = true
+
+        const response = await axios.post('http://localhost:8090/api/login', this.loginForm)
+        
+        if (response.data.token) {
+          localStorage.setItem('token', response.data.token)
+          localStorage.setItem('user', JSON.stringify(response.data.user))
+          ElMessage.success('登录成功')
+          
+          // 获取重定向地址，如果没有则返回首页
+          const redirect = this.$route.query.redirect || '/'
+          this.$router.push(redirect)
+        }
+      } catch (error) {
+        if (error.response?.data?.error) {
+          ElMessage.error(error.response.data.error)
+        } else {
+          ElMessage.error('登录失败，请检查用户名和密码')
+        }
+      } finally {
+        this.loading = false
+      }
+    },
+    goToRegister() {
+      this.$router.push('/register')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f3f3f3;
+  z-index: 1000;
+}
+
+.login-box {
+  width: 400px;
+  padding: 40px;
+  background: white;
+  border-radius: 8px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+
+.login-box h2 {
+  text-align: center;
+  margin-bottom: 30px;
+  color: #409EFF;
+}
+
+.login-button {
+  width: 100%;
+  margin-bottom: 10px;
+}
+</style>
